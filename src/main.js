2b2b1f6d9d0e0e83ec6394c515f01f7a44f53a52
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-11-28 12:59:53 -0300

    enlaces de botones

--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 // Este es el punto de entrada de tu aplicacion
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+// cambio de pantallas por botones
+document.getElementById('crearCuenta').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('inicio').style.display = 'none';
+  document.getElementById('logIn2').style.display = 'block';
+});
+
+document.getElementById('ingresar').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('inicio').style.display = 'none';
+  document.getElementById('logIn').style.display = 'block';
+});
+
+document.getElementById('ir').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('logIn').style.display = 'none';
+  document.getElementById('pagPrincipal').style.display = 'block';
+});
+
+document.getElementById('ir2').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('logIn2').style.display = 'none';
+  document.getElementById('pagPrincipal').style.display = 'block';
+});
+
+document.getElementById('regresar').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('logIn').style.display = 'none';
+  document.getElementById('inicio').style.display = 'block';
+});
+
+document.getElementById('regresar2').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('logIn2').style.display = 'none';
+  document.getElementById('inicio').style.display = 'block';
+});
