3b34ac1b6d1cf4dd9a5be5127040bf8e09d4caed
Silvana Gonzalez <algomio2000@gmail.com>
2022-12-06 18:49:31 -0500

    actualizando archivo

--- src/lib/ingresar.js
+++
@@ -1,7 +0,0 @@
- export const ingresar = (changeRouter)=>{
-    const contenedorIng= document.createElement('div');
-    const botonIr2 = document.createElement('button');
-
-    contenedorIng.append(botonIr2);
-    return ingresar;
-};
