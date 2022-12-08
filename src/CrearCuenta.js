845132e36b5d63988574e14d4b6dd57a26715e82
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-12-07 22:21:04 -0300

    ojo de password y arreglos css

--- src/CrearCuenta.js
+++ src/CrearCuenta.js
@@ -7,12 +7,9 @@ export const crearCuenta = (changeRouter) => {
   contenedorCrea.innerHTML = `
     <img src="imagenes/patitasArr.png" class="patitasArr">
     <img src="imagenes/cerdo.png" class="animal">
-    <h2>Crear Cuenta</h2>
-</header>
-<main>
-  <div>
-    <p class="frase1"> Crea una cuenta con tu correo</p>
-    <div>`;
+     <div>
+      <p class="frase1"> Crea una cuenta con tu correo</p>
+     <div>`;
   const contenedorBotones = document.createElement('div');
   contenedorBotones.classList.add('logIn3');
   const nickName = document.createElement('input');
@@ -32,17 +29,30 @@ export const crearCuenta = (changeRouter) => {
   password.id = 'password';
   password.type = 'password';
   password.name = 'password';
-  password.placeholder = 'clave';
+  /* password.minlength = '6 required';
+  password.placeholder = 'clave'; */
+  const BtnOjo = document.createElement('button');
+  BtnOjo.textContent = 'Mostrar';
+  BtnOjo.classList.add('Ojo');
   const BtnIr2 = document.createElement('button');
   BtnIr2.textContent = 'Ir';
   BtnIr2.classList.add('ir');
   const imgGoogle = document.createElement('div');
   imgGoogle.classList.add('imgGoogle');
   imgGoogle.innerHTML = `
-    <img src="imagenes/google.png">`;
+    <img src="imagenes/google.png" class="imgGoogle">`;
   const Btngoogle = document.createElement('button');
   Btngoogle.textContent = 'Ingresa con tu cuenta';
   Btngoogle.classList.add('google');
+
+  BtnOjo.addEventListener('click', () => {
+    const tipo = document.getElementById('password');
+    if (tipo.type === 'password') {
+      tipo.type = 'text';
+    } else {
+      tipo.type = 'password';
+    }
+  });
 
   BtnIr2.addEventListener('click', () => {
     const userEmail = email.value;
@@ -61,10 +71,12 @@ export const crearCuenta = (changeRouter) => {
           alert('Este usuario ya se encuentra registrado', 'error');
         } else if (error.code === 'auth/invalid-email') {
           alert('El correo ingresado no es válido');
-        } else if (error.code === 'auth/weak-password') {
-          alert('La clave debe tener al menos seis caracteres');
         } else if (error.code) {
           alert('El usuario o la clave son incorrectos');
+        } else if (password.length < 6) {
+          alert('Password minimo 6 caracteres');
+        } else if (error.code === 'auth/weak-password') {
+          alert('La clave debe tener al menos seis caracteres');
         }
       });
   });
@@ -72,6 +84,7 @@ export const crearCuenta = (changeRouter) => {
   contenedorBotones.appendChild(nickName);
   contenedorBotones.appendChild(email);
   contenedorBotones.appendChild(password);
+  contenedorBotones.appendChild(BtnOjo);
   contenedorBotones.appendChild(BtnIr2);
   contenedorBotones.appendChild(imgGoogle);
   contenedorBotones.appendChild(Btngoogle);
