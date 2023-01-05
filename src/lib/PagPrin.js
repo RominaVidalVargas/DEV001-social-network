a1788d45e584854d196ea41056ea048ff95b5f71
Silvana Gonzalez <algomio2000@gmail.com>
2023-01-05 18:19:23 -0500

    muro listo, funciones listas

--- src/lib/PagPrin.js
+++ src/lib/PagPrin.js
@@ -62,15 +62,15 @@ export function pagPrincipal() {
     }));
     // editar publicaciones
     const Btneditar = ContenPubli.querySelectorAll('.Btneditar');
-   Btneditar.forEach((Btn1) => {
-      Btn1.addEventListener('click', async (e) => {
+   Btneditar.forEach((Btn) => {
+      Btn.addEventListener('click', async (e) => {
         try {
           const doc = await getNote(e.target.dataset.id);
           const nota = doc.data();
-          WritePubli['subject'].value = nota.subject;
+          WritePubli.firstElementChild['subject'].value = nota.subject;
           editStatus = true;
           id = doc.id;
-          WritePubli['submit'].innerText = 'Update';
+          WritePubli.firstElementChild['submit'].innerText = 'Update';
         } catch (error) {
           console.log(error);
         }
@@ -96,13 +96,13 @@ export function pagPrincipal() {
 
         editStatus = false;
         id = '';
-        WritePubli['btn-task-form'].innerText = 'Save';
+        WritePubli.firstElementChild['submit'].innerText = 'Save';
       }
 console.log(WritePubli);
       WritePubli.firstElementChild.reset();
       subject.focus();
     } catch (error) {
-      console.log(error);
+      //console.log(error);
     }
   });
 
